0d998b6aa1969bbaf7aeaae7e84eba2c6057298a
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2024-06-07 11:27:41 +0100

    Initial Commit

---
+++ FaceTek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceTek
+//
+//  Created by Akintayo A. Olusegun on 07/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceTek/FaceTekApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceTekApp.swift
+//  FaceTek
+//
+//  Created by Akintayo A. Olusegun on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceTekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
